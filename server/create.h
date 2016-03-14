9d61e063d1e5d36c2aa3a0d57cd8bfc51a1744c8
Jeshua Morrissey <jeshuamorrissey@gmail.com>
2016-03-15 10:25:01 +1100

    cleaned up warnings in act.c and act.h (and probably many other files)

--- server/create.h
+++ server/create.h
@@ -17,18 +17,18 @@ extern int used_items;
 
 
 int init_create(void);
-int create_item(char *name);
-int create_char_nr(int tmp,int tmpa);
-int create_char(char *name,int tmpa);
+int create_item(const char *name);
+int create_char_nr(int tmp, int tmpa);
+int create_char(const char *name, int tmpa);
 void update_char(int cn);
 int alloc_char(void);
 void free_char(int cn);
 int getfirst_char(void);
 int getnext_char(int cn);
-void copy_char(struct character *dst,struct character *src);
+void copy_char(struct character *dst, struct character *src);
 int alloc_item(void);
 void free_item(int in);
 int destroy_items(int cn);
-int lookup_char(char *name);
-int lookup_item(char *name);
+int lookup_char(const char *name);
+int lookup_item(const char *name);
 int create_item_nr(int tmp);
