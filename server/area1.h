9784f25b1daef42201b2ea8909508c65e3f9c17c
Jeshua Morrissey <jeshuamorrissey@gmail.com>
2016-03-15 10:41:25 +1100

    cleaned up area* files

--- server/area1.h
+++ server/area1.h
@@ -20,41 +20,41 @@ Added RCS tags
 
 struct area1_ppd
 {
-	int yoakin_state;
-	int yoakin_seen_timer;
+  int yoakin_state;
+  int yoakin_seen_timer;
 
-	int gwendy_state;
-	int gwendy_seen_timer;
+  int gwendy_state;
+  int gwendy_seen_timer;
 
-	int terion_state;
+  int terion_state;
 
-	int james_state;
-	int flags;
+  int james_state;
+  int flags;
 
-	int darkin_state;	
+  int darkin_state;
 
-	int gerewin_state;
-	int gerewin_seen_timer;
+  int gerewin_state;
+  int gerewin_seen_timer;
 
-	int nook_state;
+  int nook_state;
 
-	int lydia_state;
-	int lydia_seen_timer;
+  int lydia_state;
+  int lydia_seen_timer;
 
-	int asturin_state;
-	int asturin_seen_timer;
+  int asturin_state;
+  int asturin_seen_timer;
 
-	int guiwynn_state;
-	int guiwynn_seen_timer;
+  int guiwynn_state;
+  int guiwynn_seen_timer;
 
-	int logain_state;
-	int logain_seen_timer;
+  int logain_state;
+  int logain_seen_timer;
 
-	int reskin_state;
-	int reskin_seen_timer;
-	int reskin_got_bits;
+  int reskin_state;
+  int reskin_seen_timer;
+  int reskin_got_bits;
 
-	int shrike_state;
-	int shrike_fails;
+  int shrike_state;
+  int shrike_fails;
 };
 
