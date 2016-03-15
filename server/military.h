f5cc674697c6e649861be713f2bd355e7d82ac19
Jeshua Morrissey <jeshuamorrissey@gmail.com>
2016-03-15 16:41:01 +1100

    cleanup

--- server/military.h
+++ server/military.h
@@ -16,35 +16,35 @@ Added RCS tags
 
 struct mission
 {
-	int type;
-	int opt1,opt2;
-	int pts;
-	int exp;
+  int type;
+  int opt1, opt2;
+  int pts;
+  int exp;
 };
 
 struct military_ppd
 {
-	int current_pts;	// unused 'recommendation' pts
-	int master_state;
+  int current_pts;	// unused 'recommendation' pts
+  int master_state;
 
-	int current_advisor;	// re-using storage ID
-	int advisor_state;
-	int advisor_cost;
-	int advisor_storage_nr;
+  int current_advisor;	// re-using storage ID
+  int advisor_state;
+  int advisor_cost;
+  int advisor_storage_nr;
 
-	int advisor_last[MAXADVISOR];
+  int advisor_last[MAXADVISOR];
 
-	int military_pts;	// exp gained towards ranks
-	int normal_exp;		// exp given out
+  int military_pts;	// exp gained towards ranks
+  int normal_exp;		// exp given out
 
-	int mission_yday;	// day of the year the missions were created
-	struct mission mis[5];	// easy,normal,hard,impossible,insane
+  int mission_yday;	// day of the year the missions were created
+  struct mission mis[5];	// easy,normal,hard,impossible,insane
 
-	int took_mission;	// he accepted mission 1...5
-	int took_yday;		// he took the mission on this day of the year
-	int solved_mission;	// bool: mission was solved
-	int solved_yday;	// solved the mission from yday (to avoid giving out more than one quest per day)
+  int took_mission;	// he accepted mission 1...5
+  int took_yday;		// he took the mission on this day of the year
+  int solved_mission;	// bool: mission was solved
+  int solved_yday;	// solved the mission from yday (to avoid giving out more than one quest per day)
 
-	int recommend;		// to remember if we mentioned a recommendation already
+  int recommend;		// to remember if we mentioned a recommendation already
 };
 
